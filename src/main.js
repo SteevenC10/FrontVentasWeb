938f4b5667e9135b5b015f200a8a455785cbbe20
SteevenC10 <curipallosteeven7@gmail.com>
2025-12-01 17:59:26 -0500

    Creacion Carpetas

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Punto de inicio de la aplicación
+// Aquí se configura e inicia la app
+
+console.log('Front Ventas Web iniciado');
+
+// Aquí podrías inicializar controladores, cargar datos, etc.
